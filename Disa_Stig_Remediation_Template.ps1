792b08c47c8f38cd55a16447253efd6c5c90aef5
Loughton Bennett <ftlion76.tunnels@slmail.me>
2026-02-02 08:50:46 +0100

    Create Disa_Stig_Remediation_Template.ps1

---
+++ Disa_Stig_Remediation_Template.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the associated STIG-ID vulnerability is remediated.
+
+.NOTES
+    Author          : Loughton Bennett
+    Date Created    : 02/01/2026
+    Last Modified   : 02/01/2026
+    Version         : 1.0
+    STIG-ID         : 
+
+.TESTED ON
+    Date(s) Tested  : 10/29/2025
+    Tested By       : Loughton Bennett
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1.26100.7705
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\stig_remediation_template(STIG-ID-).ps1
+
+.Description: 
+#>
+
+# YOUR CODE GOES HERE
